b76644425befac3a6ced9470cd803b3e4961b620
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2024-01-02 21:02:01 -0300

    Main atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'dart:math';
-
+import './card.dart';
 import 'package:expenses_project/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import './form.dart';
 import './transaction_list.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final ThemeData tema = ThemeData();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       home: MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.deepPurple,
+        ),
+      ),
     );
   }
 }
@@ -25,10 +39,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   final transaction = [];
+  bool error = false;
 
   void addTransaction(String title, double value) {
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
     final newTransaction = Transaction(
         id: Random().nextDouble().toString(),
         title: title,
@@ -44,21 +62,39 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
         title: const Text("Despesas Pessoais"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                backgroundColor: Colors.transparent,
+                builder: (BuildContext context) {
+                  return Container(
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    child: FormWidget(
+                        changeTransaction: addTransaction, error: error),
+                  );
+                },
+              );
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.2,
-            child: const Card(
-              elevation: 5,
-              color: Colors.deepPurple,
-              child: Text("Grafico"),
-            ),
-          ), 
-          transaction.length == 0 ? Container(child: Expanded(child: Center(child: const Text("Nenhuma transação cadastrada!")))) :
-          Transaction_List(transaction: transaction.cast<Transaction>())
+          CardWidget(),
+          SizedBox(height: 12),
+          transaction.isEmpty
+              ? Container(
+                  child: const Expanded(
+                      child:
+                          Center(child: Text("Nenhuma transação cadastrada!"))))
+              : Transaction_List(transaction: transaction.cast<Transaction>())
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -72,11 +108,11 @@ class _MyHomePageState extends State<MyHomePage> {
             builder: (BuildContext context) {
               return Container(
                 height: MediaQuery.of(context).size.height * 0.8,
-                child: FormWidget( changeTransaction: addTransaction
-                    ),
+                child:
+                    FormWidget(changeTransaction: addTransaction, error: error),
               );
             },
-          )
+          ),
         },
       ),
     );
